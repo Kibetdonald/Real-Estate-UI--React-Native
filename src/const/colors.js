07bcbbaea0d45724358cb017e7b8dcafc338bf65
Don <kibetdonald97@gmail.com>
2022-06-23 11:13:12 +0300

    Initial Commit

---
+++ src/const/colors.js
@@ -0,0 +1,12 @@
+// Arrays of colors
+const COLORS = {
+    white: '#FFF',
+    dark: '#000',
+    light: '#f6f6f6',
+    grey: '#A9A9A9',
+    blue: '#5f82e6',
+    red: 'red',
+    tranparent: 'rgba(0,0,0,0)',
+  };
+  
+  export default COLORS;
