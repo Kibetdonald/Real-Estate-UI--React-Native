1bef4d2e1a9828bbd4f8bb793f48b7c0a5538f08
Don <kibetdonald97@gmail.com>
2022-07-18 14:42:05 +0300

    Fixed react-native-reanimated/plugin error

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import "react-native-gesture-handler";
 import React from "react";
+import Transitioning from "react-native-reanimated"
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import DetailsScreen from "./src/screens/DetailsScreen";
 import OnBoardScreen from "./src/screens/OnBoardScreen";
 // import ChallengeOne from "./src/screens/ChallengeOne";
-// import ProductsScreen from "./src/screens/ProductsScreen";
+import ProductsScreen from "./src/screens/ProductsScreen";
 const Stack = createStackNavigator();
 
 const App = () => {
@@ -14,7 +15,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {/* <Stack.Screen name="ChallengeOne" component={ChallengeOne} /> */}
-        {/* <Stack.Screen name="Products Screen" component={ProductsScreen}/> */}
+        <Stack.Screen name="Products Screen" component={ProductsScreen}/>
         <Stack.Screen name="OnBoardScreen" component={OnBoardScreen} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
--- src/screens/ProductsScreen.js
+++ src/screens/ProductsScreen.js
@@ -1,11 +1,9 @@
-import { React, useState, useEffect } from "react";
-import { View, Text, SafeAreaView } from "react-native";
-import axios from "axios";
-// import { Card } from "react-native-elements";
+import React, {useState, useEffect} from 'react'
+import {View, Text, Image} from 'react-native'
+import axios from 'axios'
 
-const ProductsScreen = () => {
-  const [data, setData] = useState([]);
-
+export default function ProductsScreen() {
+  const [data, setData] = useState([])
   useEffect(() => {
     axios
       .get("http://127.0.0.1:8000/eventdetails/")
@@ -15,28 +13,21 @@ const ProductsScreen = () => {
       })
       .catch((err) => console.log(err));
   }, []);
-
-  //   Base url for the image
   const BASE_URL = "http://127.0.0.1:8000/";
-
   return (
-    <SafeAreaView>
-      <Text>Don</Text>
-      {data.map((data) => (
+    <View>
+       {data.map((data) => (
         <View key={data.id}>
           <View>
+          <Image source = {{uri:`${BASE_URL}${data.event_banner}`}}
+   style = {{ width: 200, height: 200 }}
+   />
             <Text>{data.event_name}</Text>
-            <img
-              style={{ width: "200px" }}
-              alt="Banner"
-              src={`${BASE_URL}${data.event_banner}`}
-            />
             <Text>{data.start_date}</Text>
           </View>
         </View>
       ))}
-    </SafeAreaView>
-  );
-};
 
-export default ProductsScreen
+    </View>
+  )
+}
